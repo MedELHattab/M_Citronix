2d10a0671dd40b47e48651f6a2f184e973ab277b
MedELHattab <136116042+MedELHattab@users.noreply.github.com>
2024-11-19 14:22:21 +0100

    Initial commit

---
+++ src/main/java/org/example/m_citronix/MCitronixApplication.java
@@ -0,0 +1,13 @@
+package org.example.m_citronix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MCitronixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MCitronixApplication.class, args);
+    }
+
+}
